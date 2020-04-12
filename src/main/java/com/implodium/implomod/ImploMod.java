1dd826b236fc2b87ec950edcd70b8aa7b4117323
FelixWoess <felix.woess@aon.at>
2020-04-12 02:37:56 +0200

    Wtf Crafting 2

--- src/main/java/com/implodium/implomod/ImploMod.java
+++ src/main/java/com/implodium/implomod/ImploMod.java
@@ -1,12 +1,14 @@
 package com.implodium.implomod;
 
 import com.implodium.implomod.proxy.CommonProxy;
+import net.minecraft.init.Items;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = ImploConstants.MODID, name = ImploConstants.NAME, version = ImploConstants.VERSION)
 public class ImploMod {
