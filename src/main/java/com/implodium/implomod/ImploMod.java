d832c3c0c2666ba968ee945625f545840d950e4d
Quirin <quirin.ecker@bajupa.com>
2020-04-26 03:39:56 +0200

    finished registration of the block (?)

--- src/main/java/com/implodium/implomod/ImploMod.java
+++ src/main/java/com/implodium/implomod/ImploMod.java
@@ -1,17 +1,26 @@
 package com.implodium.implomod;
 
 import com.implodium.implomod.proxy.CommonProxy;
-import net.minecraft.init.Items;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 @Mod(modid = ImploConstants.MODID, name = ImploConstants.NAME, version = ImploConstants.VERSION)
 public class ImploMod {
+    public static HashMap<String, Block> blocks = new HashMap<>();
+    public static HashMap<String, Item> items = new HashMap<>();
 
     @Mod.Instance
     private static  ImploMod instance;
@@ -36,5 +45,22 @@ public class ImploMod {
     @EventHandler
     public void postinit(FMLPostInitializationEvent event) {
         proxy.postinit(event);
+    }
+
+    private void preInitBlock(Block block) {
+        blocks.put("infinite_water", block);
+        ForgeRegistries.BLOCKS.register(blocks.get("infinite_water"));
+
+        ItemBlock itemBlock = new ItemBlock(block);
+        items.put("infinite_water", itemBlock);
+        itemBlock.setRegistryName(Objects.requireNonNull(block.getRegistryName()));
+        ForgeRegistries.ITEMS.register(itemBlock);
+
+        ModelResourceLocation modelResourceLocation = new ModelResourceLocation(
+                ImploConstants.MODID + ":" + block.getRegistryName(),
+                "inventory"
+        );
+
+        ModelLoader.setCustomModelResourceLocation(itemBlock, 0, modelResourceLocation);
     }
 }
