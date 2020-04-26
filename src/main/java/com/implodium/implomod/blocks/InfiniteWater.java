de0b3efe2457015d06a6b018a5ea3b05b4b8aac5
Quirin <quirin.ecker@bajupa.com>
2020-04-26 03:30:54 +0200

    created class for planned block infiniteWater

---
+++ src/main/java/com/implodium/implomod/blocks/InfiniteWater.java
@@ -0,0 +1,27 @@
+package com.implodium.implomod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class InfiniteWater extends Block {
+    public static final String NAME = "infinite_water";
+    private static final String UNLOCALIZED_NAME = "infinite_water_x";
+    private static final String REGISTRY_NAME = "infinite_water";
+
+    public InfiniteWater() {
+        super(Material.GLASS);
+
+        this.setCreativeTab(CreativeTabs.BREWING);
+        this.setUnlocalizedName(UNLOCALIZED_NAME);
+        this.setRegistryName(REGISTRY_NAME);
+    }
+
+    @SideOnly(value = Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.SOLID;
+    }
+}
