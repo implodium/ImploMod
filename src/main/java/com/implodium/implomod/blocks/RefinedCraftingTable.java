c54df9e3bdb64545a69d197e4f045ff6809eb61f
FelixWoess <felix.woess@aon.at>
2020-04-28 21:18:12 +0200

    new Block
    removed:
    (with faulty Bounding/Collision Box)

--- src/main/java/com/implodium/implomod/blocks/RefinedCraftingTable.java
+++ src/main/java/com/implodium/implomod/blocks/RefinedCraftingTable.java
@@ -3,18 +3,28 @@ package com.implodium.implomod.blocks;
 import com.implodium.implomod.init.ModBlocks;
 import com.implodium.implomod.init.ModItems;
 import javafx.scene.chart.Axis;
+import net.minecraft.block.BlockWorkbench;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.IInteractionObject;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -24,7 +34,7 @@ import java.util.Random;
 
 public class RefinedCraftingTable extends BlockBase {
 
-    public static final AxisAlignedBB REFINED_CRAFTING_TABLE_BB = new AxisAlignedBB(1.0D, 0.0D, 1.0D, 1.0D, 1.0625D, 1.0D);
+    //public static final AxisAlignedBB REFINED_CRAFTING_TABLE_BB = new AxisAlignedBB(1.0D, 0.0D, 1.0D, 1.0D, 1.0625D, 1.0D);
 
     public RefinedCraftingTable(String name, Material material) {
         super(name, material);
@@ -58,13 +68,13 @@ public class RefinedCraftingTable extends BlockBase {
         return EnumBlockRenderType.MODEL;
     }
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return REFINED_CRAFTING_TABLE_BB;
-    }
+    //@Override
+    //public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    //  return REFINED_CRAFTING_TABLE_BB;
+    //}
 
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-        return REFINED_CRAFTING_TABLE_BB;
-    }
+    //@Override
+    //public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+    //    return REFINED_CRAFTING_TABLE_BB;
+    //}
 }
