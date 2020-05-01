389b37edfacf73a9872778c5def07f7985e99520
FelixWoess <felix.woess@aon.at>
2020-05-01 03:39:34 +0200

    new Item + Crafting

--- src/main/java/com/implodium/implomod/init/ModItems.java
+++ src/main/java/com/implodium/implomod/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.implodium.implomod.init;
 
 import com.implodium.implomod.items.ImploShard;
+import com.implodium.implomod.items.RFTransmitter;
 import com.implodium.implomod.items.RefinedGold;
 import com.implodium.implomod.items.RefinedIron;
 import net.minecraft.item.Item;
@@ -15,4 +16,5 @@ public class ModItems
     public static final Item IMPLO_SHARD = new ImploShard("implo_shard");
     public static final Item REFINED_GOLD = new RefinedGold("refined_gold_ingot");
     public static final Item REFINED_IRON = new RefinedIron("refined_iron_ingot");
+    public static final Item RF_TRANSMITTER = new RFTransmitter("rf_transmitter");
 }
---
+++ src/main/java/com/implodium/implomod/items/RFTransmitter.java
@@ -0,0 +1,13 @@
+package com.implodium.implomod.items;
+
+public class RFTransmitter extends ItemBase
+{
+
+    public RFTransmitter(String name)
+    {
+        super(name);
+
+        this.setMaxStackSize(64);
+    }
+
+}
